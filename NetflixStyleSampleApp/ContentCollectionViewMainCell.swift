d46e5418aa899d999540926fdd74ed7e4b1ceae9
min-kyoung <shalsrud9@naver.com>
2022-01-12 21:27:07 +0900

    snap kit + preview + 배너 + 리스트

---
+++ NetflixStyleSampleApp/ContentCollectionViewMainCell.swift
@@ -0,0 +1,163 @@
+//
+//  ContentCollectionViewMainCell.swift
+//  NetflixStyleSampleApp
+//
+//  Created by 노민경 on 2022/01/12.
+//
+
+import UIKit
+
+class ContentCollectionViewMainCell: UICollectionViewCell {
+    let baseStackView = UIStackView()
+    let menuStackView = UIStackView()
+    
+    let btnTV = UIButton()
+    let btnMovie = UIButton()
+    let btnCategory = UIButton()
+    
+    let imageView = UIImageView()
+    let descriptionLabel = UILabel()
+    let contentStackView = UIStackView()
+    
+    let btnPlus = UIButton()
+    let btnPlay = UIButton()
+    let btnInfo = UIButton()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        [baseStackView, menuStackView].forEach {
+            contentView.addSubview($0)
+        }
+        
+        // baseStackView layout
+        baseStackView.axis = .vertical // 축
+        baseStackView.alignment = .center
+        baseStackView.distribution = .fillProportionally
+        baseStackView.spacing = 5
+        
+        // imageView 추가
+        [imageView, descriptionLabel, contentStackView].forEach {
+            baseStackView.addArrangedSubview($0)
+        }
+        
+        // imageView 설정
+        imageView.contentMode = .scaleAspectFit
+        imageView.snp.makeConstraints {
+            $0.width.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(imageView.snp.width) // 너비와 높이가 같은 1:1의 정사각형 모양
+        }
+        
+        // descriptionLabel
+        descriptionLabel.font = .systemFont(ofSize: 13)
+        descriptionLabel.textColor = .white
+        descriptionLabel.sizeToFit()
+        
+        // contentStackView
+        contentStackView.axis = .horizontal
+        contentStackView.alignment = .center
+        contentStackView.distribution = .equalCentering
+        contentStackView.spacing = 20
+        
+        // contentStackView autolayout
+        contentStackView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(30)
+        }
+        
+        // contentStackView에 서브 요소 추가
+        [btnPlus, btnInfo].forEach {
+            $0.titleLabel?.font = .systemFont(ofSize: 13)
+            $0.setTitleColor(.white, for: .normal)
+            $0.imageView?.tintColor = .white
+            $0.adjustVerticalLayout(5)
+        }
+        
+        btnPlus.setTitle("내가 찜한 컨텐츠", for: .normal)
+        btnPlus.setImage(UIImage(systemName: "plus"), for: .normal)
+        btnPlus.addTarget(self, action: #selector(btnPlusTapped), for: .touchUpInside)
+        
+        btnInfo.setTitle("정보", for: .normal)
+        btnInfo.setImage(UIImage(systemName: "info.circle"), for: .normal)
+        btnInfo.addTarget(self, action: #selector(btnInfoTapped), for: .touchUpInside)
+        
+        btnPlay.setTitle("재생", for: .normal)
+        btnPlay.setTitleColor(.blue, for: .normal)
+        btnPlay.backgroundColor = .white
+        btnPlay.layer.cornerRadius = 3
+        btnPlay.snp.makeConstraints {
+            $0.width.equalTo(90)
+            $0.height.equalTo(30)
+        }
+        btnPlay.addTarget(self, action: #selector(btnPlayTapped), for: .touchUpInside)
+        
+        // 버튼 추가
+        [btnPlus, btnPlay, btnInfo].forEach {
+            contentStackView.addArrangedSubview($0)
+            
+        }
+        
+        // baseStackView autolayout
+        baseStackView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(30)
+            $0.height.equalTo(60)
+        }
+        
+        // menuStackView layout
+        menuStackView.axis = .horizontal // 축
+        menuStackView.alignment = .center
+        menuStackView.distribution = .equalSpacing
+        menuStackView.spacing = 20
+        
+        // 버튼 추가
+        [btnTV, btnMovie, btnCategory].forEach {
+            menuStackView.addArrangedSubview($0)
+            $0.setTitleColor(.white, for: .normal)
+            $0.layer.shadowColor = UIColor.black.cgColor
+            $0.layer.shadowOpacity = 1
+            $0.layer.shadowRadius = 3
+        }
+        
+        btnTV.setTitle("TV 프로그램", for: .normal)
+        btnMovie.setTitle("영화", for: .normal)
+        btnCategory.setTitle("카테고리", for: .normal)
+        
+        // 버튼에 액션 추가
+        btnTV.addTarget(self, action: #selector(btnTVTapped), for: .touchUpInside)
+        btnMovie.addTarget(self, action: #selector(btnMovieTapped), for: .touchUpInside)
+        btnCategory.addTarget(self, action: #selector(btnCategoryTapped), for: .touchUpInside)
+        
+        // autolayout
+        menuStackView.snp.makeConstraints {
+            $0.top.equalTo(baseStackView)
+            $0.leading.trailing.equalToSuperview().inset(30)
+        }
+        
+        
+        
+    }
+    
+    @objc func btnTVTapped(sender: UIButton!) {
+        print("TEST :  TV Button Tapped")
+    }
+    
+    @objc func btnMovieTapped(sender: UIButton!) {
+        print("TEST :  Movie Button Tapped")
+    }
+    
+    @objc func btnCategoryTapped(sender: UIButton!) {
+        print("TEST :  Category Button Tapped")
+    }
+    
+    
+    @objc func btnPlusTapped(sender: UIButton!) {
+        print("TEST :  Plus Button Tapped")
+    }
+    
+    @objc func btnInfoTapped(sender: UIButton!) {
+        print("TEST :  Info Button Tapped")
+    }
+    
+    @objc func btnPlayTapped(sender: UIButton!) {
+        print("TEST :  Play Button Tapped")
+    }
+}
--- NetflixStyleSampleApp/HomeViewController.swift
+++ NetflixStyleSampleApp/HomeViewController.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 class HomeViewController: UICollectionViewController {
     var contents: [Content] = []
+    var mainItem: Item?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +26,16 @@ class HomeViewController: UICollectionViewController {
         
         // Data 설정, 가져오기
         contents = getContents()
+        mainItem = contents.first?.contentItem.randomElement() // 첫번째 컨텐츠에서 랜덤으로 요소를 뽑아옴
         
         // CollectionView Item(Cell) 설정
         collectionView.register(ContentCollectionViewCell.self, forCellWithReuseIdentifier: "ContentCollectionViewCell")
         collectionView.register(ContentCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ContentCollectionViewHeader")
         
         collectionView.register(ContentCollectionViewRankCell.self, forCellWithReuseIdentifier: "ContentCollectionViewRankCell")
+        
+        collectionView.register(ContentCollectionViewMainCell.self, forCellWithReuseIdentifier: "ContentCollectionViewMainCell")
+    
         
         // 생성한 layout을 CollectionView에 적용
         collectionView.collectionViewLayout = layout()
@@ -56,6 +61,8 @@ class HomeViewController: UICollectionViewController {
                 return self.createLargeTypeSection()
             case .rank:
                 return self.createRankTypeSection()
+            case .main:
+                return self.createMainTypeSection()
             default:
                 return nil
             }
@@ -107,6 +114,23 @@ class HomeViewController: UICollectionViewController {
         
     }
     
+    // Main Section Layout 설정
+    private func createMainTypeSection() -> NSCollectionLayoutSection {
+        // item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        // group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(450))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 0, leading: 0, bottom: 20, trailing: 0)
+        
+        return section
+    }
+    
     // Section Header Layout 설정
     private func createSectionHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
         // Section Header 사이즈
@@ -146,15 +170,12 @@ class HomeViewController: UICollectionViewController {
 extension HomeViewController {
     // 섹션당 보여질 셀의 개수
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if contents[section].sectionType == .basic || contents[section].sectionType == .large || contents[section].sectionType == .rank {
             switch section {
             case 0:
                 return 1
             default:
                 return contents[section].contentItem.count
             }
-        }
-        return 0
     }
     
     // collectionView 셀 설정
@@ -175,9 +196,13 @@ extension HomeViewController {
             
             return cell
             
-        default:
-            return UICollectionViewCell()
+        case .main:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCollectionViewMainCell", for: indexPath) as? ContentCollectionViewMainCell else { return UICollectionViewCell() }
             
+            cell.imageView.image = mainItem?.image
+            cell.descriptionLabel.text = mainItem?.description
+            
+            return cell
         }
     }
     
